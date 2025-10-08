66b7497436b6badb10b793b7dbeaeee81445d010
gnnpdr <annmark2006@mail.ru>
2025-10-08 19:03:33 +0300

    belady works with tests. But just one file and comments

--- main.cpp
+++ main.cpp
@@ -5,72 +5,95 @@
 
 const size_t MAX_CACHE_SIZE = 200;
 
-class IdealCache
+class BeladyCache
 {
 private:
 
     struct CacheCell
     {
-        int key;        // = val
+        int key;
         int next_pos;
 
-        bool operator<(const CacheCell& other) const        //чтобы можно было сравнивать
+        bool operator<(const CacheCell& other) const
         {
-            return next_pos < other.next_pos;
+            return next_pos <= other.next_pos;
         }
     };
 
-    std::vector<int> requests;      //список запросов элементов
+    std::vector<int> requests;
     std::set<CacheCell> cache_set;
-    std::unordered_map<int, std::set<CacheCell>::iterator> cells_table;         //обеспечивает быстрый доступ к элементам кэша, по ключу дается ссылка на ячейку
+    std::unordered_map<int, std::set<CacheCell>::iterator> cells_table; 
     size_t capacity;
+    size_t cur_pos = 0;
+    size_t match_cnt = 0;
 
-    using iterator = std::set<CacheCell>::iterator;
+    using the_cell = std::set<CacheCell>::iterator;
 
 public:
 
-    IdealCache(size_t capacity, std::vector<int> requests) : capacity(capacity), requests(requests) {}
+    BeladyCache(size_t capacity, std::vector<int> requests) : capacity(capacity), requests(requests) {}
 
-    void cache_push(int key, size_t pos)
+    size_t cache_push(int key)
     {
-        iterator new_cell_it;
+        cur_pos++;
+        the_cell new_cell_it;
+        //std::cout << "pos: " << pos << std::endl;
+        //std::cout << "START size " << cache_set.size() << " cap " << capacity << std::endl;
+        
         auto it = cells_table.find(key);
         if (it == cells_table.end())
         {
-            //промах, нужно проверить, есть ли свободное место
+            //std::cout << "dis " << key << " pos " << cur_pos << std::endl;
+            
             if (cache_set.size() >= capacity)
             {
-                //места нет, надо вытеснить, нам просто найти элемент с самым больщим значением next_use, потому что мы используем set, этот элемемнт будет в самом начале
-                auto del_cell = cache_set.begin();
+                auto del_cell = --cache_set.end();
+                //std::cout << "del " << del_cell->key << std::endl;
                 cells_table.erase(del_cell->key);
                 cache_set.erase(del_cell);
             }
 
-            //теперь нужно добавить новый элемент, set автоматически его поставит, нужно только засунуть его туда
-            //у нас уже есть конструктор, теперь используем список иницаализации, создаем новую ячейку
-            CacheCell new_cell{key, find_next_pos(key, pos + 1)};
-            new_cell_it = cache_set.insert(new_cell).first;    //first - итератор, second - bool был ли вставлен
-            
+            if (find_next_pos(key, cur_pos + 1) != MAX_CACHE_SIZE)
+            {
+                CacheCell new_cell{key, find_next_pos(key, cur_pos + 1)};           //нельзя, чтобы функция запрашивала позицию, может, стоит сделать эту переменную глобальной
+                new_cell_it = cache_set.insert(new_cell).first;
+            }
+            else
+            {
+                CacheCell new_cell{key, MAX_CACHE_SIZE};           //нельзя, чтобы функция запрашивала позицию, может, стоит сделать эту переменную глобальной
+                new_cell_it = cache_set.insert(new_cell).first;
+            }
         }
         else
         {
-            //нужно поменять в нужной ячейке значение следующего появления
-            //но это set -> мы не можем просто заменить значение по которому идет упорядочивание, нам нужно стереть предыдущее поле и вставить новое
+            //std::cout << "match " << key << " pos " << cur_pos << std::endl;
+            match_cnt++;
             CacheCell the_cell = *it->second;
             cache_set.erase(it->second);
-
-            the_cell.next_pos = find_next_pos(key, pos + 1);
+            if (find_next_pos(key, cur_pos + 1) != MAX_CACHE_SIZE)
+            {
+                the_cell.next_pos = find_next_pos(key, cur_pos + 1);
+            }
+            else
+            {
+                //надо копировать элемент? он удаляется и новый не создаеется
+                the_cell =  {key, MAX_CACHE_SIZE};
+            }
             new_cell_it = cache_set.insert(the_cell).first;
         }
         cells_table[key] = new_cell_it;
 
-    }
+        //std::cout << "END size " << cache_set.size() << " cap " << capacity << std::endl;
+        print_cache();
+        std::cout << std::endl;
+
+        return match_cnt;
+    } 
 
 private:
 
     int find_next_pos(int key, size_t pos)
     {
-        //от заданной позиции пройти по следующим элементам до конца, пока не кончится вектор
         for (int i = pos; i < requests.size(); i++)
         {
             if (key == requests[i])
@@ -79,4 +102,129 @@ private:
 
         return MAX_CACHE_SIZE;
     }
+
+    void print_cache()
+    {
+
+        for (const auto& element : cache_set)
+        {
+            std::cout << element.key << " " ;
+        }
+
+        std::cout << std::endl;
+    }
 };
+
+class TestRunner
+{
+private:
+    size_t total_test_amt = 0;
+    size_t passed_test_amt = 0;
+
+public:
+
+    void test(size_t res, size_t expected_res)
+    {
+        total_test_amt++;
+        if (res == expected_res)
+            passed_test_amt++;
+        else
+        {
+            std::cout << "test " << total_test_amt << " failed. ";
+            std::cout << "expected " << expected_res;
+            std::cout << ". received " << res << std::endl;
+        }
+    }
+
+    void print_results()
+    {
+        if (total_test_amt != passed_test_amt)
+        {
+            std::cout << passed_test_amt << " out of " << total_test_amt << " tests passed\nDO SOMETHING WITH IT" << std::endl;
+
+        }
+        else
+            std::cout << "ALL TESTS PASSED CONGRATULATIONS!" << std::endl;
+    }
+};
+
+void run_Belady_tests()
+{
+    TestRunner runner;
+    size_t matches = 0;
+
+    size_t el_amt = 6;
+    std::vector<int> reqs1 = {1, 2, 1, 2, 1, 2};
+    BeladyCache cache1(2, reqs1);
+
+    for (int i = 0; i < 2; i++)         //проверяем прогрев кэша
+    {
+        int k = reqs1[i];
+        matches = cache1.cache_push(k);
+    }
+    runner.test(matches, 0);
+
+    for (int i = 2; i < el_amt; i++)        //проверяем количество попаданий в конкретном примере
+    {
+        int k = reqs1[i];
+        matches = cache1.cache_push(k);
+    }
+    runner.test(matches, 4);
+
+
+    std::cout << "NEW TEST" << std::endl;
+    std::cout << std::endl;
+    matches = 0;
+    el_amt = 10;
+    std::vector<int> reqs2 = {1, 2, 3, 1, 2, 3, 4, 4, 4, 7};
+    BeladyCache cache2(5, reqs2);
+    for (int i = 0; i < el_amt; i++)
+    {
+        int k = reqs2[i];
+        matches = cache2.cache_push(k);
+    }
+    runner.test(matches, 5);
+
+    std::cout << "NEW TEST" << std::endl;
+    std::cout << std::endl;
+    matches = 0;
+    el_amt = 9;
+    std::vector<int> reqs3 = {1, 2, 3, 7, 2, 5, 3, 5, 1};
+    BeladyCache cache3(3, reqs3);
+    for (int i = 0; i < el_amt; i++)
+    {
+        int k = reqs3[i];
+        matches = cache3.cache_push(k);
+    }
+    runner.test(matches, 3);
+
+    runner.print_results();
+}
+
+
+int main()
+{//
+    //size_t cap = 0;
+    //std::cin >> cap;
+    //size_t el_amt = 0;
+    //std::cin >> el_amt;
+    //std::vector<int> reqs;
+    //for (int i = 0; i < el_amt; i++)
+    //{
+    //    int k = 0;
+    //    std::cin >> k;
+    //    reqs.push_back(k);
+    //}
+    //BeladyCache cache(cap, reqs);
+    //size_t matches = 0;
+    //for (int i = 0; i < el_amt; i++)
+    //{
+    //    int k = reqs[i];
+    //    matches = cache.cache_push(k);
+    //}
+    //std::cout << "matches: " << matches << std::endl; 
+
+    run_Belady_tests();
+    
+    return 0;
+}
